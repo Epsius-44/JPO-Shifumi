4b063186ea1e18b4e16263de7aa0895ac215e0a9
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-02-26 17:02:56 +0100

    Add start/restart game

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
-    return 0
+    relance_partie = True
+    while relance_partie:
+        saisie = input('Voulez vous lancer une partie de Pierre / Feuille / Ciseaux ? (O)ui / (N)on : ').upper()
+        if saisie in ['O', 'N']:
+            if saisie == 'O':
+                # Boucle de jeu
+                return 0
+            else:
+                relance_partie = False
+        else:
+            print("Saisie incorrect !")
 
 
 main()
